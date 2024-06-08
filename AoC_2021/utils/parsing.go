ec129c50a6b89a69c97263ce6d5be227a826a638
J3y0 <91211461+J3y0@users.noreply.github.com>
2024-06-08 16:54:51 +0200

    feat: AOC_2021 - Add day1, day2, day3

---
+++ AoC_2021/utils/parsing.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+func ParseLines(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(strings.ReplaceAll(string(data), "\r\n", "\n"), "\n")
+	return lines, nil
+}
