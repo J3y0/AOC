e598d1d253714acf03fa9a21f2b61306c7993335
J3y0 <91211461+J3y0@users.noreply.github.com>
2025-12-02 00:05:19 +0100

    feat: add day01 solutions

--- AoC_2025/src/days/day01.rs
+++ AoC_2025/src/days/day01.rs
@@ -3,15 +3,81 @@ use crate::days::Solution;
 pub struct Day01;
 
 impl Solution for Day01 {
-    type Input = ();
+    type Input = Vec<i16>;
 
-    fn parse(&self, data: &str) -> Self::Input {}
+    fn parse(&self, data: &str) -> Self::Input {
+        data.lines()
+            .map(|line| {
+                let dir = line.chars().nth(0).unwrap();
+                let parsed = (&line[1..]).parse::<i16>().unwrap();
+                if dir == 'L' { -parsed } else { parsed }
+            })
+            .collect()
+    }
 
     fn part1(&self, input: &Self::Input) -> usize {
-        0
+        // Start at position 50
+        let mut cur = 50;
+        let mut passwd = 0;
+        for elt in input {
+            cur = (cur + elt).rem_euclid(100);
+            if cur == 0 {
+                passwd += 1;
+            }
+        }
+
+        passwd
     }
 
     fn part2(&self, input: &Self::Input) -> usize {
-        0
+        // Start at position 50
+        let mut cur = 50;
+        let mut passwd = 0;
+        for &elt in input {
+            // add all additional complete rotations
+            passwd += ((cur + elt) / 100).abs();
+
+            // Add if point 0 by subtracting the value
+            if elt < 0 && cur != 0 && -elt >= cur {
+                passwd += 1;
+            }
+
+            cur = (cur + elt).rem_euclid(100);
+        }
+
+        passwd as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example_data() -> Vec<i16> {
+        let sol = Day01 {};
+        sol.parse(
+            r#"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"#,
+        )
+    }
+
+    #[test]
+    fn part1_test() {
+        let sol = Day01 {};
+        assert_eq!(sol.part1(&example_data()), 3);
+    }
+
+    #[test]
+    fn part2_test() {
+        let sol = Day01 {};
+        assert_eq!(sol.part2(&example_data()), 6);
     }
 }
--- AoC_2025/src/main.rs
+++ AoC_2025/src/main.rs
@@ -9,18 +9,23 @@ use std::{fs::read_to_string, process, time::Instant};
 fn run(day: usize, sol: impl Solution) {
     let path = format!("input/{day:02}_day.txt");
     if let Ok(data) = read_to_string(&path) {
-        let now = Instant::now();
         let input = sol.parse(&data);
+
+        let now = Instant::now();
         let part1 = sol.part1(&input);
+        let elapsed_part1 = now.elapsed();
+
+        let now = Instant::now();
         let part2 = sol.part2(&input);
-        let elapsed = now.elapsed();
+        let elapsed_part2 = now.elapsed();
 
         println!("{YELLOW}------------{RESET}");
         println!("{BOLD}{YELLOW}   Day {day:02}{RESET}");
         println!("{YELLOW}------------{RESET}");
         println!("Part1: {part1}");
+        println!("Time1: {} ns", elapsed_part1.as_nanos());
         println!("Part2: {part2}");
-        println!("Time: {} ns", elapsed.as_nanos());
+        println!("Time2: {} ns", elapsed_part2.as_nanos());
     } else {
         println!("{RED}------------{RESET}");
         println!("{BOLD}{RED}   Day {day:02}{RESET}");
